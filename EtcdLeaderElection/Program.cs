0e182bcb30e2124c59ec8277a9b4830d624318d6
Andriy Stelmakh <stelmakha@ryanair.com>
2021-02-18 14:54:31 +0200

    leader election working

--- EtcdLeaderElection/Program.cs
+++ EtcdLeaderElection/Program.cs
@@ -1,69 +1,107 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using dotnet_etcd;
 using Etcdserverpb;
 using Google.Protobuf;
-using Google.Protobuf.Collections;
+using Grpc.Core;
+using Mvccpb;
 
 namespace EtcdLeaderElection
 {
     class Program
     {
         private const long Ttl = 5;
-        private const string LeaderKey = "Leader_Key";
-
+        private const string LeaderKey = "/leader";
+        private static readonly EtcdClient EtcdClient = new EtcdClient("http://localhost:2379");
+        private static long Lease;
+        
         static async Task Main(string[] args)
         {
             var serverName = args[0];
-            var etcdClient = new EtcdClient("http://localhost:2379");
 
             while (true)
             {
-                var leaderResult = await ElectLeader(etcdClient, serverName);
+                var leaderResult = await ElectLeader(serverName);
                 if (leaderResult.Item1)
                 {
                     Console.WriteLine("I am the leader.");
-                    await LeadershipGained(etcdClient, leaderResult.Item2);
+                    Console.CancelKeyPress += ConsoleOnCancelKeyPress;
+                    await LeadershipGained(leaderResult.Item2);
                 }
                 else
                 {
                     Console.WriteLine("I am a follower.");
-                    await WitForNextElection(etcdClient);
+                    await WitForNextElection();
                 }
             }
         }
 
-        private static async Task WitForNextElection(EtcdClient etcdClient)
+        private static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("Revoking leader lease");
+
+            EtcdClient.LeaseRevokeAsync(new LeaseRevokeRequest()
+            {
+                ID = Lease
+            });
+            
+            Environment.Exit(-1);
+        }
+
+        private static async Task WitForNextElection()
         {
             var request = new WatchRequest()
             {
                 CreateRequest = new WatchCreateRequest()
                 {
                     Key = ByteString.CopyFromUtf8(LeaderKey)
-                }
+                },
             };
 
-            await etcdClient.Watch(request, print);
+            Console.WriteLine("Waiting for leader to die.");
+            
+            var cancellationTokeSource = new CancellationTokenSource();
+            
+            try
+            {
+                await EtcdClient.Watch(request, async response =>
+                {
+                    foreach (var @event in response.Events)
+                    {
+                        if (@event.Type == Event.Types.EventType.Delete)
+                        {
+                            Console.WriteLine("Leader died. Cancelling watch.");
+                            cancellationTokeSource.Cancel();
+                        }
+                    }
+                }, cancellationToken: cancellationTokeSource.Token);
+            }
+            catch (RpcException)
+            {
+                Console.WriteLine("Watch cancelled");
+            }
         }
-        
-        private static void print(WatchResponse response)
-        {   
-        }
-
-        private static async Task LeadershipGained(EtcdClient etcdClient, long lease)
+        private static async Task LeadershipGained(long lease)
         {
             while (true)
             {
                 try
                 {
                     Console.WriteLine("Refreshing. Still a leader.");
-                    //var response =  await etcdClient.LeaseTimeToLiveAsync(new LeaseTimeToLiveRequest());
+                    await EtcdClient.LeaseKeepAlive(new LeaseKeepAliveRequest()
+                    {
+                        ID = lease
+                    }, response =>
+                    {
+
+                    }, CancellationToken.None);
                     await DoWork();
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Revoking lease. No longer a leader.");
-                    await etcdClient.LeaseRevokeAsync(new LeaseRevokeRequest() {ID = lease});
+                    await EtcdClient.LeaseRevokeAsync(new LeaseRevokeRequest() {ID = lease});
                 }
             }
         }
@@ -73,20 +111,22 @@ namespace EtcdLeaderElection
             return Task.Delay(1000);
         }
 
-        private static async Task<(bool, long)> ElectLeader(EtcdClient etcdClient, string serverName)
+        private static async Task<(bool, long)> ElectLeader(string serverName)
         {
             Console.WriteLine("Electing a leader.");
             
-            var leaseResponse = await etcdClient.LeaseGrantAsync(new LeaseGrantRequest
+            var leaseResponse = await EtcdClient.LeaseGrantAsync(new LeaseGrantRequest
             {
                 TTL = Ttl
             });
 
-            var put = await TryPut(etcdClient, LeaderKey, serverName, leaseResponse.ID);
+            Lease = leaseResponse.ID;
+
+            var put = await TryPut(LeaderKey, serverName, Lease);
             return (put, leaseResponse.ID);
         }
 
-        private static async Task<bool> TryPut(EtcdClient etcdClient, string key, string value, long lease)
+        private static async Task<bool> TryPut(string key, string value, long lease)
         {
             var request = new TxnRequest();
             request.Success.Add(new RequestOp
@@ -99,8 +139,9 @@ namespace EtcdLeaderElection
                 }
             });
             request.Compare.Add(new Compare {Version = 0, Key = ByteString.CopyFromUtf8(key)});
+            request.Failure.Add(new RequestOp[0]);
          
-            var response = await etcdClient.TransactionAsync(request);
+            var response = await EtcdClient.TransactionAsync(request);
             return response.Succeeded;
         }
     }
